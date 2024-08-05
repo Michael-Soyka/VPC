6b1c02d0900393765e422553928942bb20c46e2a
Michael-Soyka <hihihi.good.67@gmail.com>
2024-08-05 04:51:38 +0300

    Added git commit sha if building with GHA

--- utils/vpc/vpc.cpp
+++ utils/vpc/vpc.cpp
@@ -178,17 +178,18 @@ bool CVPC::Init(int argc, const char **argv) {
         SetConsoleTitle( "Valve Project Creator" );
     #endif
 
-    Log_Msg(LOG_VPC, "VPC - Valve Project Creator for ");
-    #ifdef GIT_HASH
+    Log_Msg( LOG_VPC, "VPC - Valve Project Creator for " );
+    Log_Msg( LOG_VPC, "Visual Studio and Make\n" );
+    #ifdef GIT_SHA
         Log_Msg( LOG_VPC, 
-            "Visual Studio and Make ( Build time: %s %s; Build commit: %s )\n", __DATE__, __TIME__, GIT_HASH );
+            "Build time: %s %s; Build commit: %s\n", __DATE__, __TIME__, GIT_SHA );
     #else
         Log_Msg( LOG_VPC, 
-            "Visual Studio and Make ( Build: %s %s )\n", __DATE__, __TIME__ );
+            "Build: %s %s\n", __DATE__, __TIME__ );
     #endif
-    Log_Msg(LOG_VPC,
-            "(C) Copyright 1996-2024, Valve Corporation, All rights reserved.\n");
-    Log_Msg(LOG_VPC, "\n");
+    Log_Msg( LOG_VPC,
+            "(C) Copyright 1996-2024, Valve Corporation, All rights reserved.\n" );
+    Log_Msg( LOG_VPC, "\n" );
 
     return true;
 }
